b159bbc33fc83ca436010209a9876b61b4f39fa5
Boucqueau <gyboucqueau@hotmail.com>
2020-05-11 18:26:24 +0200

    set a new state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = (props) => {
-    return (
+class Contact extends React.Component {
+
+    state = {
+        online: false
+    }
+    
+    handleChange() {
+        this.setState(prevState => ({online: ! prevState.online}))
+    }
+
+    render () {
+
+    const {...props} = this.props;
+
+        return (
     <div  className="Contact">
         <img src={props.avatar} className="avatar" alt="" />
         <div>
             <p className="name">{props.name}</p>
-            <div className="status"> 
-                <div className={props.online ? "status-online" : "status-offline"}/>
-                <p className="status-text">{props.online ? "online" : "offline"}</p>
+            <div className="status" onClick={()  => this.handleChange()}> 
+                <div className={this.state.online ? "status-online" : "status-offline"}/>
+                <p className="status-text">{this.state.online ? "online" : "offline"}</p>
             </div>
         </div>    
     </div>  
-    )
+    )}  
 }
 
 export default Contact;
